aced3900dfb3625777ffb48d834831cae26c3f8f
Paras Waghela <paraswaghela777@gmail.com>
2024-07-22 00:23:47 +0530

    deployed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://shopping-cart-frontend-one.vercel.app',
     credentials: true
 }));
 
